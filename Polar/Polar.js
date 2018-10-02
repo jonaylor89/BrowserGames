3a888268f05603de9dfd9604631ba6d2d086c4ad
John Naylor <jonaylor89@gmail.com>
2018-10-02 14:16:06 -0400

    Add es6 let

--- Polar/Polar.js
+++ Polar/Polar.js
@@ -7,9 +7,9 @@ function setup(){
 }
 function draw(){
 
-  var n = 5;
-  var d = 7;
-  var k = n/d;
+  let n = 5;
+  let d = 7;
+  let k = n/d;
 
   translate(width/2, height/2);
   stroke(255);
@@ -18,10 +18,10 @@ function draw(){
   beginShape();
   noFill();
 
-  for(var i = 0; i <= TWO_PI * d; i += 0.01){
-    var r = 100 * cos(k * i);
-    var x = r * cos(i);
-    var y = r * sin(i);
+  for(let i = 0; i <= TWO_PI * d; i += 0.01){
+    let  r = 100 * cos(k * i);
+    let x = r * cos(i);
+    let y = r * sin(i);
 
     vertex(x, y);
   }
