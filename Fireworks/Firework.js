643aece6a775be08c9911124ea00fb25b4b45cb9
gapgag55 <imkopkap@gmail.com>
2018-10-05 13:10:11 +0700

    ES6 Fireworks

--- Fireworks/Firework.js
+++ Fireworks/Firework.js
@@ -1,54 +1,47 @@
+class Firework {
+  constructor() {
+    this.firework = new Particle(random(width), height);
+    this.particles = [];
+    this.exploded = false;
+  }
 
-function Firework(){
-
-  this.firework = new Particle(random(width), height);
-
-  this.particles = [];
-  this.exploded = false;
-
-  this.update = function(){
-    if(!this.exploded){
+  update() {
+    if (!this.exploded) {
       this.firework.update();
       this.firework.applyForce(gravity);
-        if(this.firework.vel.y >= 0){
-          this.exploded = true;
-          this.explode();
-        }
-
+      if (this.firework.vel.y >= 0) {
+        this.exploded = true;
+        this.explode();
+      }
     }
 
-    for(var y = this.particles.length - 1; y >= 0; y--){
+    for (var y = this.particles.length - 1; y >= 0; y--) {
       this.particles[y].applyForce(gravity);
       this.particles[y].update();
 
-      if(this.particles[y].done()){
-
+      if(this.particles[y].done()) {
           this.particles.splice(y, 1);
-
       }
     }
   };
 
-  this.explode = function(){
-
-      for(var x = 0; x < 75; x++){
-        var p = new Particle(this.firework.pos.x, this.firework.pos.y, true);
-        this.particles.push(p);
-
-      }
-
+  explode() {
+    const {x, y} = this.firework.pos;
+    for (let x = 0; x < 75; x++) {
+      let p = new Particle(x, y, true);
+      this.particles.push(p);
+    }
   };
 
-  this.done = function(){
+  done() {
     return (this.exploded && this.particles.length === 0)
   };
 
-  this.show = function(){
-    if(!this.exploded){
+  show() {
+    if (!this.exploded)
       this.firework.show();
-    }
-    for(var y = this.particles.length - 1; y >= 0; y--){
+
+    for(let y = this.particles.length - 1; y >= 0; y--)
       this.particles[y].show();
-    }
   };
 }
